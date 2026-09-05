7605e4bca5072338a8231b1ba9e06c2ab6fc6f51
aliii390 <allli.said.errahmani@gmail.com>
2026-09-05 02:50:51 +0200

    add: changemand cdn chart par symfony chart

--- src/Controller/JournalController.php
+++ src/Controller/JournalController.php
@@ -16,17 +16,59 @@ use Symfony\UX\Chartjs\Model\Chart;
 final class JournalController extends AbstractController
 {
     #[Route('/journal', name: 'app_journal')]
-    public function index(JournalRepository $journalRepo): Response
-    {
-        $user = $this->getUser();
+public function index(JournalRepository $journalRepo, ChartBuilderInterface $chartBuilder): Response
+{
+    $user = $this->getUser();
+    $journal = $journalRepo->findBy(['user' => $user]);
 
-        $journal = $journalRepo->findBy(['user'=> $user]) ;
+    // On remet dans l'ordre chronologique (du plus ancien au plus récent) pour le graphique
+    $entrees = array_reverse($journal);
 
+    $labels = [];
+    $valeurs = [];
+    foreach ($entrees as $entree) {
+        $labels[] = $entree->getDate()->format('d/m');
+        $valeurs[] = $entree->getIntensite();
+    }
 
-        return $this->render('journal/index.html.twig', [
-            'infoJournal' => $journal,
-        ]);
-        }
+    $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
+    $chart->setData([
+        'labels' => $labels,
+        'datasets' => [[
+            'label' => 'Intensité',
+            'data' => $valeurs,
+            'borderColor' => '#111827',
+            'backgroundColor' => '#111827',
+            'borderWidth' => 2,
+            'pointRadius' => 4,
+            'pointBackgroundColor' => '#111827',
+            'tension' => 0.3,
+            'fill' => false,
+        ]],
+    ]);
+    $chart->setOptions([
+        'responsive' => true,
+        'maintainAspectRatio' => false,
+        'plugins' => ['legend' => ['display' => false]],
+        'scales' => [
+            'y' => [
+                'beginAtZero' => true,
+                'max' => 10,
+                'ticks' => ['stepSize' => 2, 'color' => '#9CA3AF'],
+                'grid' => ['color' => '#F3F4F6'],
+            ],
+            'x' => [
+                'ticks' => ['color' => '#9CA3AF'],
+                'grid' => ['display' => false],
+            ],
+        ],
+    ]);
+
+    return $this->render('journal/index.html.twig', [
+        'infoJournal' => $journal,
+        'chart' => $chart,
+    ]);
+}
 
 
 

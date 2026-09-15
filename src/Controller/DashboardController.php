6a37d0c30761f9a34c38f863f751def3cfa4e3a1
aliii390 <allli.said.errahmani@gmail.com>
2026-09-15 20:09:34 +0200

    add: correction

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\MedicamentRepository;
 use App\Repository\RendezVousRepository;
+use App\Repository\JournalRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,17 +12,20 @@ use Symfony\Component\Routing\Attribute\Route;
 final class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
-    public function index(RendezVousRepository $rdvRepo, MedicamentRepository $medicamentRepo): Response
+    public function index(RendezVousRepository $rdvRepo, MedicamentRepository $medicamentRepo, JournalRepository $journalRepo): Response
     {
-
         $user = $this->getUser();
         $rdv = $rdvRepo->findBy(['user' => $user]);
-        $medoc = $medicamentRepo->findBy(['user'=>$user]);
+        $medoc = $medicamentRepo->findBy(['user' => $user]);
 
+        // Dernière entrée du journal (la plus récente)
+        $dernierJournal = $journalRepo->findBy(['user' => $user], ['date' => 'DESC'], 1);
+        $dernierJournal = $dernierJournal[0] ?? null;
 
         return $this->render('dashboard/index.html.twig', [
             'rendezVous' => $rdv,
             'medicaments' => $medoc,
+            'dernierJournal' => $dernierJournal,
         ]);
     }
 }
